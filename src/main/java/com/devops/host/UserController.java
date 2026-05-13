1e585bb2f273010d5c9101f8be6ea39000aeedfe
maha17p <maha17p@gmail.com>
2026-05-13 07:58:46 +0530

    controller response is changed

--- src/main/java/com/devops/host/UserController.java
+++ src/main/java/com/devops/host/UserController.java
@@ -8,6 +8,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
     @GetMapping
     public String user(){
-        return "user logged in";
+        return "user logged in ....";
     }
 }
